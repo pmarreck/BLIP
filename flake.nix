e673ccfef50d0e0caa46dd2da785840078c40343
Peter Marreck <lumbergh@gmail.com>
2026-03-04 17:25:31 -0500

    chore: migrate flake.nix to fixed-output derivation pattern
    Replace pre-fetched tarballs + --system with a zigDeps fixed-output
    derivation that uses `zig build --fetch=all`. Simpler to maintain
    and automatically handles transitive dependencies. Add separate
    checks.default for tests and Darwin-specific build inputs.

--- flake.nix
+++ flake.nix
@@ -10,54 +10,29 @@
     flake-utils.lib.eachDefaultSystem (system:
       let
         pkgs = nixpkgs.legacyPackages.${system};
+        pname = "blip";
+        version = "0.2.0";
+        isDarwin = pkgs.stdenv.isDarwin;
 
-        # Pre-fetch Zig dependencies for sandbox-compatible builds
-        z7z-src = pkgs.fetchurl {
-          url = "https://github.com/pmarreck/z7z/archive/93112f05573ba294b18f98ec5473251b7fedd240.tar.gz";
-          hash = "sha256-LgKhIr141lq+UZQ7Xv3r0u3UczNJs8Sly29Y9OFnJMQ=";
-        };
-        bzip2z-src = pkgs.fetchurl {
-          url = "https://github.com/pmarreck/bzip2z/archive/76d3ca8f35dad2c1393eb35d97332d5a8e9da2bb.tar.gz";
-          hash = "sha256-rkST0lDO7szyRsbIZRr4s9btyd79A5oVS5Q2eiO0PCw=";
-        };
-        lz4-src = pkgs.fetchurl {
-          url = "https://github.com/pmarreck/lz4/archive/675532cbcc0609ddfc44a6e13240902574579a25.tar.gz";
-          hash = "sha256-OQH1Fkaru+D2vThSE2tEJ+HQ07Koe9tEQ6EKPBzjeZ8=";
-        };
-        progrez-src = pkgs.fetchurl {
-          url = "https://github.com/pmarreck/progrez/archive/377256d88ef8e664177b88a66a361a46be4e4c83.tar.gz";
-          hash = "sha256-q7omg5xppVn5iIgl9VVYKqOMDiea5tm6KbBd0GUiy4E=";
-        };
-        zstdz-src = pkgs.fetchurl {
-          url = "https://github.com/pmarreck/zstdz/archive/c79c202b8d6512a72a5b0b65ad771adfda66d301.tar.gz";
-          hash = "sha256-Uq/M87sBrp1VB/Ahr8Pu4vE3AMgR/2nHjJQT18XfH8o=";
-        };
-        # Transitive dependency of z7z
-        libmagic-src = pkgs.fetchurl {
-          url = "https://github.com/pmarreck/libmagic/archive/refs/tags/zig-0.15.0.tar.gz";
-          hash = "sha256-lEySLP0ththmbc/zRhbGlKcPk5H0jvGzDMohOYGmJqg=";
-        };
+        zigDepsHash = "sha256-xLCFr0yspJN8NvZqMDpcTQrY5ivjBsFEz9bL47wNcdI=";
 
-        # Build a Zig system package directory from pre-fetched dependencies.
-        # Zig's --system flag expects: <pkgdir>/<zig-hash>/...
         zigDeps = pkgs.stdenv.mkDerivation {
-          name = "blip-zig-deps";
-          dontUnpack = true;
+          pname = "${pname}-zig-deps";
+          inherit version;
+          src = self;
+          nativeBuildInputs = with pkgs; [ zig git cacert ];
+          outputHashMode = "recursive";
+          outputHashAlgo = "sha256";
+          outputHash = zigDepsHash;
           buildPhase = ''
-            mkdir -p $out/z7z-0.1.0-rkKuF1v8BQAz2YEHTrHZ3KrgSvHwfPfiwiX3HOAPmF4Y
-            tar xzf ${z7z-src} --strip-components=1 -C $out/z7z-0.1.0-rkKuF1v8BQAz2YEHTrHZ3KrgSvHwfPfiwiX3HOAPmF4Y
-            mkdir -p $out/bzip2z-0.1.0-m5NdlvkqAwCEafj8KkeEu4fIXSTRp-sKQewGM28tYrcr
-            tar xzf ${bzip2z-src} --strip-components=1 -C $out/bzip2z-0.1.0-m5NdlvkqAwCEafj8KkeEu4fIXSTRp-sKQewGM28tYrcr
-            mkdir -p $out/lz4-1.10.0-TtaqjVLWBwDiQxASdpBmT-44zoqcVnVkA9kQGAonGWDf
-            tar xzf ${lz4-src} --strip-components=1 -C $out/lz4-1.10.0-TtaqjVLWBwDiQxASdpBmT-44zoqcVnVkA9kQGAonGWDf
-            mkdir -p $out/progrez-0.1.0-0YJXrl0CAgCI-gvYCSEWEnW2rsWMTPbehyOnOveB4EJp
-            tar xzf ${progrez-src} --strip-components=1 -C $out/progrez-0.1.0-0YJXrl0CAgCI-gvYCSEWEnW2rsWMTPbehyOnOveB4EJp
-            mkdir -p $out/zstdz-1.6.0-yDWjzshaNABOkMCYfrjAhKjPt46sKjdR9cLB-XgzeTt4
-            tar xzf ${zstdz-src} --strip-components=1 -C $out/zstdz-1.6.0-yDWjzshaNABOkMCYfrjAhKjPt46sKjdR9cLB-XgzeTt4
-            mkdir -p $out/libmagic-5.46.0-RysxHD9fCACu5caBjXS-x3qQnQM3nRfosCyktdVRzv-R
-            tar xzf ${libmagic-src} --strip-components=1 -C $out/libmagic-5.46.0-RysxHD9fCACu5caBjXS-x3qQnQM3nRfosCyktdVRzv-R
+            export HOME=$TMPDIR
+            export ZIG_GLOBAL_CACHE_DIR=$out
+            export SSL_CERT_FILE=${pkgs.cacert}/etc/ssl/certs/ca-bundle.crt
+            export GIT_SSL_CAINFO=${pkgs.cacert}/etc/ssl/certs/ca-bundle.crt
+            zig build --fetch=all
           '';
           dontInstall = true;
+          dontFixup = true;
         };
       in {
         devShells.default = pkgs.mkShell {
@@ -68,27 +43,48 @@
         };
 
         packages.default = pkgs.stdenv.mkDerivation {
-          pname = "blip";
-          version = "0.2.0";
-          src = ./.;
-          nativeBuildInputs = [ pkgs.zig ];
+          inherit pname version;
+          src = self;
+          nativeBuildInputs = [ pkgs.zig ]
+            ++ pkgs.lib.optionals isDarwin [
+              pkgs.darwin.cctools
+              pkgs.apple-sdk
+            ];
           dontConfigure = true;
           dontInstall = true;
-          doCheck = true;
+          dontFixup = true;
           buildPhase = ''
-            mkdir -p .cache
-            zig build \
-              --cache-dir $(pwd)/.cache \
-              --global-cache-dir $(pwd)/.cache \
-              --system ${zigDeps} \
-              -Doptimize=ReleaseFast \
-              --prefix $out
+            export HOME="$TMPDIR"
+            export ZIG_GLOBAL_CACHE_DIR=$TMPDIR/zig-cache
+            mkdir -p $ZIG_GLOBAL_CACHE_DIR
+            cp -r ${zigDeps}/* $ZIG_GLOBAL_CACHE_DIR/
+            chmod -R u+w $ZIG_GLOBAL_CACHE_DIR
+            zig build --prefix $out -Doptimize=ReleaseFast
           '';
-          checkPhase = ''
-            zig build test \
-              --cache-dir $(pwd)/.cache \
-              --global-cache-dir $(pwd)/.cache \
-              --system ${zigDeps}
+        };
+
+        checks.default = pkgs.stdenv.mkDerivation {
+          pname = "${pname}-tests";
+          inherit version;
+          src = self;
+          nativeBuildInputs = [ pkgs.zig ]
+            ++ pkgs.lib.optionals isDarwin [
+              pkgs.darwin.cctools
+              pkgs.apple-sdk
+            ];
+          dontConfigure = true;
+          dontFixup = true;
+          buildPhase = ''
+            export HOME="$TMPDIR"
+            export ZIG_GLOBAL_CACHE_DIR=$TMPDIR/zig-cache
+            mkdir -p $ZIG_GLOBAL_CACHE_DIR
+            cp -r ${zigDeps}/* $ZIG_GLOBAL_CACHE_DIR/
+            chmod -R u+w $ZIG_GLOBAL_CACHE_DIR
+            timeout 600 zig build test || { echo "Tests failed"; exit 1; }
+          '';
+          installPhase = ''
+            mkdir -p $out
+            echo "tests passed" > $out/result
           '';
         };
       }
