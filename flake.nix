11cf6b419521a8f71f7e00fd8ae42f11b40d07dc
Peter Marreck <lumbergh@gmail.com>
2026-05-14 18:55:24 -0400

    flake: pin Zig 0.16.0 via mitchellh/zig-overlay (fix CI on Garnix)

--- flake.nix
+++ flake.nix
@@ -4,12 +4,17 @@
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
     flake-utils.url = "github:numtide/flake-utils";
+    zig-overlay = {
+      url = "github:mitchellh/zig-overlay";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
 
-  outputs = { self, nixpkgs, flake-utils }:
+  outputs = { self, nixpkgs, flake-utils, zig-overlay }:
     flake-utils.lib.eachDefaultSystem (system:
       let
         pkgs = nixpkgs.legacyPackages.${system};
+        zig = zig-overlay.packages.${system}."0.16.0";
         pname = "blip";
         version = "3.0.0";
         isDarwin = pkgs.stdenv.isDarwin;
@@ -24,7 +29,7 @@
         packages.default = pkgs.stdenv.mkDerivation {
           inherit pname version;
           src = self;
-          nativeBuildInputs = [ pkgs.zig ]
+          nativeBuildInputs = [ zig ]
             ++ pkgs.lib.optionals isDarwin [
               pkgs.darwin.cctools
               pkgs.apple-sdk
@@ -44,7 +49,7 @@
           pname = "${pname}-tests";
           inherit version;
           src = self;
-          nativeBuildInputs = [ pkgs.zig ]
+          nativeBuildInputs = [ zig ]
             ++ pkgs.lib.optionals isDarwin [
               pkgs.darwin.cctools
               pkgs.apple-sdk
